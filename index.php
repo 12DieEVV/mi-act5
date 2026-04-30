483cac7c620bd7ee9dc923b00afb4c5826d1ceed
12DieEVV <al02987441@tecmilenio.mx>
2026-04-30 01:43:36 +0000

    Corrijo query para que no se muestre texto

--- index.php
+++ index.php
@@ -1,6 +1,31 @@
 <?php
 echo "<h1>Mi aplicación desplegada en EC2 con CodeDeploy</h1>";
 echo "<p>¡Hola mundo desde AWS!</p>";
+
+// Conexión a la base de datos
+$servername = "lab-db.coxofei5xgh6.us-east-1.rds.amazonaws.com";
+$username = "admin";
+$password = "Admin1234";
+$dbname = "lab-db";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
+}
+
+// Consulta con filtro ENTREGADO
+$sql = "SELECT nombre, estado FROM entregas WHERE estado = 'ENTREGADO'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo $row["nombre"]." - ".$row["estado"]."<br>";
+    }
+} else {
+    echo "No hay resultados";
+}
+
+$conn->close();
 ?>
-WHERE e.estado = 'ENTREGADO'
 
